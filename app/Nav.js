de421f32d5b44be60fb2d8c177b022d86e0d9344
manvithak <manvithakandimalla@gmail.com>
2018-03-29 02:07:18 -0700

    style navbar

--- app/Nav.js
+++ app/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link,HashRouter,Route,Switch} from 'react-router-dom';
+import {HashRouter,Route,Switch} from 'react-router-dom';
+import {NavLink as Link} from 'react-router-dom';
 import PersonalInformation from './modules/profile/PersonalInformation.js';
 import Address from './modules/profile/Address.js';
 import Experiance from './modules/profile/Experiance';
@@ -10,7 +11,7 @@ class Nav extends React.Component{
       <HashRouter>
         <div>
           <div className="topnav">
-            <Link to={'/'}>Personal Information</Link>
+            <Link to={'/path1'}>Personal Information</Link>
             <Link to={'/path2'}>Address</Link>
             <Link to={'/path3'}>Education Details</Link>
             <Link to={'/path4'}>Experiance</Link>
@@ -19,7 +20,7 @@ class Nav extends React.Component{
             <Route component={Address} path="/path2" />
             <Route component={EducationDetails} path="/path3" />
             <Route component={Experiance} path="/path4" />
-            <Route component={PersonalInformation} path="/" />
+            <Route component={PersonalInformation} path="/path1" />
           </Switch>
         </div>
       </HashRouter>
